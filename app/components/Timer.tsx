6364b62f8b77d8c042f33e2de0331237c3c1fdf0
Jeremy Novak <olepunchy@gmail.com>
2023-06-05 20:32:16 -0500

    Minimal app working

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -83,8 +83,27 @@ function Timer({initialMinutes, initialSeconds}: TimerProps) {
                     }
                     <span>{isActive ? 'Pause' : 'Start'}</span>
                 </button>
-                <button className="w-[140px] px-4 py-3 text-white bg-black rounded-sm"
-                        onClick={resetTimer}>Reset
+                <button
+                    className="w-[140px] flex items-center px-4 py-3 text-white bg-black rounded-sm"
+                    onClick={resetTimer}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="32" height="32"
+                        className="mr-4"
+                        viewBox="0 0 24 24">
+                        <g
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2">
+                            <path
+                                d="M10 2h4m-2 12v-4m-8 3a8 8 0 0 1 8-7a8 8 0 1 1-5.3 14L4 17.6"/>
+                            <path
+                                d="M9 17H4v5"/>
+                        </g>
+                    </svg>
+                    <span>Reset</span>
                 </button>
             </div>
         </div>
