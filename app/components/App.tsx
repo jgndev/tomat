62666ad7abae694e3dc485d3fbe73371da271aad
Jeremy Novak <olepunchy@gmail.com>
2023-06-05 20:29:58 -0500

    Minimal app working

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,56 +1,63 @@
-import { useState } from "react";
+import {useState} from "react";
 import Timer from "~/components/Timer";
 
 function App() {
 
-  const [activeTab, setActiveTab] = useState('work')
-  const [key, setKey] = useState(Date.now());
-  const workDuration = 25; // TODO: Get this from local storage
-  const shortBreakDuration = 5; // TODO: Get this from local storage
-  const longBreakDuration = 15; // TODO: Get this from local storage
+    const focus = 'focus';
+    const shortBreak = 'shortBreak';
+    const longBreak = 'longBreak';
 
-  const handleTabChange = (tab: string) => {
-    setActiveTab(tab);
-    // The `key` prop in React forces a component to unmount and remount, effectively
-    // resetting its state whenever the key changes. This helps to ensure the timer is
-    // reset when we switch tabs.
-    setKey(Date.now());
-  }
+    const [activeTab, setActiveTab] = useState(focus)
+    const [key, setKey] = useState(Date.now());
+    const workDuration = 25; // TODO: Get this from local storage
+    const shortBreakDuration = 5; // TODO: Get this from local storage
+    const longBreakDuration = 15; // TODO: Get this from local storage
 
-  return (
-    <div className="mt-8 grid place-content-center place-items-center">
-      <div className="flex justify-center items-center space-x-4">
-        <button className="w-[160px] px-4 py-3 text-zinc-100 bg-black rounded-sm"
-          onClick={() => handleTabChange('work')}>
-          Focus
-        </button>
-        <button className="w-[160px] px-4 py-3 text-zinc-100 bg-black rounded-sm"
-          onClick={() => handleTabChange('shortBreak')}>
-          Short Break
-        </button>
-        <button className="w-[160px] px-4 py-3 text-zinc-100 bg-black rounded-sm"
-          onClick={() => handleTabChange('longBreak')}>
-          Long Break
-        </button>
-      </div>
+    const handleTabChange = (tab: string) => {
+        setActiveTab(tab);
+        // The `key` prop in React forces a component to unmount and remount, effectively
+        // resetting its state whenever the key changes. This helps to ensure the timer is
+        // reset when we switch tabs.
+        setKey(Date.now());
+    }
 
-      {activeTab === 'work' &&
-        <Timer
-          key={key}
-          initialMinutes={workDuration}
-          initialSeconds={0} />}
-      {activeTab === 'shortBreak' &&
-        <Timer
-          key={key}
-          initialMinutes={shortBreakDuration}
-          initialSeconds={0} />}
-      {activeTab === 'longBreak' &&
-        <Timer
-          key={key}
-          initialMinutes={longBreakDuration}
-          initialSeconds={0} />}
-    </div>
-  )
+    return (
+        <div className="mt-8 grid place-content-center place-items-center">
+            <div className="flex justify-center items-center space-x-4">
+                <button
+                    className="w-[160px] px-4 py-3 text-zinc-100 bg-black rounded-sm"
+                    onClick={() => handleTabChange(focus)}>
+                    Focus
+                </button>
+                <button
+                    className="w-[160px] px-4 py-3 text-zinc-100 bg-black rounded-sm"
+                    onClick={() => handleTabChange(shortBreak)}>
+                    Short Break
+                </button>
+                <button
+                    className="w-[160px] px-4 py-3 text-zinc-100 bg-black rounded-sm"
+                    onClick={() => handleTabChange(longBreak)}>
+                    Long Break
+                </button>
+            </div>
+
+            {activeTab === focus &&
+                <Timer
+                    key={key}
+                    initialMinutes={workDuration}
+                    initialSeconds={0}/>}
+            {activeTab === shortBreak &&
+                <Timer
+                    key={key}
+                    initialMinutes={shortBreakDuration}
+                    initialSeconds={0}/>}
+            {activeTab === longBreak &&
+                <Timer
+                    key={key}
+                    initialMinutes={longBreakDuration}
+                    initialSeconds={0}/>}
+        </div>
+    )
 }
 
 export default App;
--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -1,64 +1,94 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import type {TimerProps} from "~/interfaces/timerProps";
 
-function Timer({ initialMinutes, initialSeconds }: TimerProps) {
+function Timer({initialMinutes, initialSeconds}: TimerProps) {
 
-  const [minutes, setMinutes] = useState(initialMinutes);
-  const [seconds, setSeconds] = useState(initialSeconds);
-  const [isActive, setIsActive] = useState(false);
+    const [minutes, setMinutes] = useState(initialMinutes);
+    const [seconds, setSeconds] = useState(initialSeconds);
+    const [isActive, setIsActive] = useState(false);
 
-  useEffect(() => {
-    let interval: NodeJS.Timeout;
-    if (isActive) {
-      interval = setInterval(() => {
-        setSeconds((seconds) => {
-          if (seconds > 0) {
-            document.title = `${minutes < 10 ? `0${minutes}` : minutes}:${(seconds - 1) < 10 ? `0${seconds - 1}` : seconds - 1} - Remaining`;
-            return seconds - 1;
-          } else if (minutes > 0) {
-            document.title = `${(minutes - 1) < 10 ? `0${minutes - 1}` : minutes - 1}:59 - Remaining`;
-            setMinutes(minutes - 1);
-            return 59;
-          } else {
+    useEffect(() => {
+        let interval: NodeJS.Timeout;
+        if (isActive) {
+            interval = setInterval(() => {
+                setSeconds((seconds) => {
+                    if (seconds > 0) {
+                        document.title = `${minutes < 10 ? `0${minutes}` : minutes}:${(seconds - 1) < 10 ? `0${seconds - 1}` : seconds - 1} - Remaining`;
+                        return seconds - 1;
+                    } else if (minutes > 0) {
+                        document.title = `${(minutes - 1) < 10 ? `0${minutes - 1}` : minutes - 1}:59 - Remaining`;
+                        setMinutes(minutes - 1);
+                        return 59;
+                    } else {
+                        clearInterval(interval);
+                        document.title = "00:00 - Time's up!";
+                        return 0;
+                    }
+                });
+            }, 1000);
+        } else {
+            document.title = `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} - Paused`;
+        }
+        return () => {
             clearInterval(interval);
-            document.title = "00:00 - Time's up!";
-            return 0;
-          }
-        });
-      }, 1000);
-    } else {
-      document.title = `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} - Paused`;
+        }
+    }, [isActive, minutes, seconds]);
+
+
+    const toggleIsActive = () => {
+        setIsActive(!isActive);
     }
-    return () => {
-      clearInterval(interval);
+
+    const resetTimer = () => {
+        setIsActive(false);
+        setMinutes(initialMinutes);
+        setSeconds(initialSeconds);
     }
-  }, [isActive, minutes, seconds]);
 
 
-  const toggleIsActive = () => {
-    setIsActive(!isActive);
-  }
-
-  const resetTimer = () => {
-    setIsActive(false);
-    setMinutes(initialMinutes);
-    setSeconds(initialSeconds);
-  }
-
-
-  return (
-    <div className="grid place-content-center place-items-center">
-      <h1 className="font-bold text-black text-[160pt]">
-        {`${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`}
-      </h1>
-      <div className="flex space-x-4">
-        <button className="w-[140px] px-4 py-3 text-white bg-black rounded-sm"
-          onClick={toggleIsActive}>{isActive ? 'Pause' : 'Start'}</button>
-        <button className="w-[140px] px-4 py-3 text-white bg-black rounded-sm"
-          onClick={resetTimer}>Reset</button>
-      </div>
-    </div>
-  )
+    return (
+        <div className="grid place-content-center place-items-center">
+            <h1 className="my-14 font-mono font-bold text-black text-[180pt]">
+                {`${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`}
+            </h1>
+            <div className="flex space-x-4">
+                <button
+                    className="w-[140px] flex items-center px-4 py-3 text-white bg-black rounded-sm"
+                    onClick={toggleIsActive}>
+                    {isActive
+                        ?
+                            // Timer is active, show pause
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="32"
+                                height="32"
+                                className="mr-4"
+                                viewBox="0 0 24 24">
+                                <path
+                                    fill="currentColor"
+                                    d="M9 16h2V8H9v8Zm4 0h2V8h-2v8Zm-1 6q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"/>
+                            </svg>
+                        :
+                            // Timer is not active, show play
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="32"
+                                height="32"
+                                className="mr-4"
+                                viewBox="0 0 24 24">
+                                <path
+                                    fill="currentColor"
+                                    d="m9.5 16.5l7-4.5l-7-4.5v9ZM12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"/>
+                            </svg>
+                    }
+                    <span>{isActive ? 'Pause' : 'Start'}</span>
+                </button>
+                <button className="w-[140px] px-4 py-3 text-white bg-black rounded-sm"
+                        onClick={resetTimer}>Reset
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default Timer;
